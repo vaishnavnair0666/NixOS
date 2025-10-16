7195bbfd716e78dcfeedac2838f67afa4fa38955
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2025-10-17 02:12:04 +0530

    a

--- modules/special/terminaltools.nix
+++ modules/special/terminaltools.nix
@@ -1,5 +1,17 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = [ pkgs.atuin ];
+
+  programs.atuin = {
+    enable = true;
+    enableBashIntegration =
+      true; # or enableBashIntegration / enableFishIntegration, depending on your shell
+    settings = {
+      # Disable up-arrow keybinding
+      keymap_mode = "vim-insert"; # optional, just keeps normal behavior
+      search_mode = "ctrl-r"; # set Atuin to only trigger with Ctrl+R
+      keymap_cursor = "none"; # disables up-arrow search
+    };
+  };
 }
