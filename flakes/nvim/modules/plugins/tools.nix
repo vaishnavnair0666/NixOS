0ac32a90a6c2f16dd4ae300317db8ee08bc4353f
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2025-11-18 04:54:16 +0530

    nixvim

--- flakes/nvim/modules/plugins/tools.nix
+++ flakes/nvim/modules/plugins/tools.nix
@@ -1,18 +1,49 @@
 { ... }: {
   programs.nixvim.plugins.ccc = {
     enable = true;
-
     settings = {
       highlighter = { auto_enable = true; };
-
       picker = {
-        default_color = "#FFFFFF";
+        default_color = "#ffffff";
         format = "hex";
+        # step config (exposed to ccc mappings via closures)
+        step = {
+          fine = 1; # used by Left/Right
+          coarse = 10; # used by Shift+Left/Shift+Right
+        };
       };
+      mappings = {
+        # navigation
+        prev_slider = "k";
+        next_slider = "j";
+        slider_left = "<Left>";
+        slider_right = "<Right>";
 
-      highlight_mode = "background";
+        # adjustments
+        dec_fine = "<Left>";
+        inc_fine = "<Right>";
+        dec_coarse = "H"; # Shift+Left alternative
+        inc_coarse = "L"; # Shift+Right alternative
 
-      alpha = 0.1;
+        # alternate adjustment keys (vi-style)
+        dec_large = "h";
+        inc_large = "l";
+
+        # format / mode
+        next_format = "i";
+        prev_format = "I";
+
+        # accept / cancel
+        accept = "<CR>";
+        cancel = "q";
+        close = "<Esc>";
+
+        # extras
+        eyedropper = "<A-p>"; # Alt-p to pick color from screen (if supported)
+        copy_hex = "<A-c>"; # Alt-c to copy hex to clipboard
+        paste_color = "<A-v>"; # Alt-v to paste color into picker
+
+      };
     };
   };
 }
