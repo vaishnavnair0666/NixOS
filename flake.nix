494d035181888def93f8ca1a11ebe9c6ca5228c3
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2025-11-28 12:57:27 +0530

    got it working with nh as well

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "My NixOS + Home Manager config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; # stable
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; # unstable
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "unstable";
@@ -14,7 +14,6 @@
     #   url = "github:abenz1267/walker";
     #   inputs.elephant.follows = "elephant";
     # };
-
     nvim.url = "path:./flakes/nvim";
     nvim.inputs.nixpkgs.follows = "unstable";
   };
@@ -27,6 +26,7 @@
         inherit system;
         config = { allowUnfree = true; };
       };
+
       unstablePkgs = import unstable {
         inherit system;
         config = { allowUnfree = true; };
@@ -35,73 +35,88 @@
       lib = nixpkgs.lib;
 
       # find host files in ./hosts 
-      hostFiles =
-        if lib.pathExists ./hosts then builtins.readDir ./hosts else [ ];
+      hostFiles = if lib.pathExists ./hosts then
+        builtins.attrNames (builtins.readDir ./hosts)
+      else
+        [ ];
       hostNames = lib.map
         (name: builtins.substring 0 (builtins.stringLength name - 4) name)
         hostFiles;
-      makeHostEntry = host:
+
+      # build a nixosSystem for a host
+      makeNixosSystem = host:
         let
           hostFilePath = ./hosts/${host}.nix;
           hostOverrideAttr = import hostFilePath;
-        in {
-          name = host;
-          value = nixpkgs.lib.nixosSystem {
-            inherit system pkgs;
-            specialArgs = {
-              inherit inputs unstablePkgs;
-              hostOverride = hostOverrideAttr;
-            };
-            modules = [
-              ./configuration.nix
-              sops-nix.nixosModules.sops
-              ./modules/secrets.nix
-              # Integrate home-manager as a NixOS module
-              home-manager.nixosModules.home-manager
-              {
-                #home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = false;
-
-                home-manager.backupFileExtension = "backup";
-                # configure your user here
-                home-manager.users.vaish = {
-                  imports =
-                    [ ./home.nix inputs.nvim.homeManagerModules.default ];
-                };
-
-                home-manager.extraSpecialArgs = { inherit unstablePkgs; };
-              }
-            ];
+        in nixpkgs.lib.nixosSystem {
+          inherit system pkgs;
+          specialArgs = {
+            inherit inputs unstablePkgs;
+            hostOverride = hostOverrideAttr;
           };
+          modules = [
+            ./configuration.nix
+            sops-nix.nixosModules.sops
+            ./modules/secrets.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useUserPackages = false;
+              home-manager.backupFileExtension = "backup";
+              home-manager.users.vaish = {
+                imports = [ ./home.nix inputs.nvim.homeManagerModules.default ];
+              };
+              home-manager.extraSpecialArgs = { inherit unstablePkgs; };
+            }
+          ];
         };
-    in {
-      # keep an explicit "nixos" entry for backwards compatibility 
-      nixosConfigurations = lib.recursiveUpdate (
-        # default single configuration retained under "nixos"
-        lib.listToAttrs [{
-          name = "nixos";
-          value = nixpkgs.lib.nixosSystem {
-            inherit system pkgs;
-            specialArgs = {
-              inherit inputs unstablePkgs;
-              hostOverride = { };
+
+      # default system entry
+      defaultNixosSystem = nixpkgs.lib.nixosSystem {
+        inherit system pkgs;
+        specialArgs = {
+          inherit inputs unstablePkgs;
+          hostOverride = { };
+        };
+        modules = [
+          ./configuration.nix
+          sops-nix.nixosModules.sops
+          ./modules/secrets.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useUserPackages = false;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.vaish = {
+              imports = [ ./home.nix inputs.nvim.homeManagerModules.default ];
             };
-            modules = [
-              ./configuration.nix
-              sops-nix.nixosModules.sops
-              ./modules/secrets.nix
-              home-manager.nixosModules.home-manager
-              {
-                home-manager.useUserPackages = false;
-                home-manager.backupFileExtension = "backup";
-                home-manager.users.vaish = {
-                  imports =
-                    [ ./home.nix inputs.nvim.homeManagerModules.default ];
-                };
-                home-manager.extraSpecialArgs = { inherit unstablePkgs; };
-              }
-            ];
-          };
-        }] ++ lib.map makeHostEntry hostNames) { };
+            home-manager.extraSpecialArgs = { inherit unstablePkgs; };
+          }
+        ];
+      };
+
+      # nixosConfigurations: default + per-host
+      nixosEntries = lib.listToAttrs ([{
+        name = "nixos";
+        value = defaultNixosSystem;
+      }] ++ (lib.map (h: {
+        name = h;
+        value = makeNixosSystem h;
+      }) hostNames));
+
+      # packages.x86_64-linux must expose the full nixosSystem object for nh to find .config.system.build.toplevel
+      packagesEntries = lib.listToAttrs ([{
+        name = "nixos";
+        value = defaultNixosSystem;
+      }] ++ (lib.map (h:
+        let ns = makeNixosSystem h;
+        in {
+          name = h;
+          value = ns;
+        }) hostNames));
+
+    in {
+      nixosConfigurations = lib.recursiveUpdate nixosEntries { };
+
+      packages = { x86_64-linux = packagesEntries; };
     };
+
 }
---
+++ hosts/asuslaptop.nix
@@ -0,0 +1,1 @@
+{ disableAtkbd = true; }
