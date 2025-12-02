04bfa55307f48ca535c26e6c5f306387c7c927e1
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2025-12-03 00:35:19 +0530

    xwayland oopsie

--- modules/windowManager.nix
+++ modules/windowManager.nix
@@ -2,6 +2,9 @@
 
 {
   programs.niri.enable = true;
-  services.xwayland.enable = true;
   programs.niri.package = pkgs.niri;
+
+  programs.xwayland-satellite.enable = true;
+
+  environment.systemPackages = with pkgs; [ xorg.xwayland ];
 }
